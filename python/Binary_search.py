486c68883160ace3f3d081b6aca7da72a13385f5
KasambaLumwagi <kasambalumwagi@gmail.com>
2024-06-19 16:40:48 +0300

    modified:   python/Binary_search.py

--- python/Binary_search.py
+++ python/Binary_search.py
@@ -2,7 +2,7 @@ def binary_search(arr,target):
     left , right = 0 , len(arr)-1
     while left<= right:
         mid = (left+ right)//2
-        if arr[mid] = target:
+        if arr[mid] == target:
             return mid
         elif arr[mid] <target :
             left = mid + 1 
@@ -10,5 +10,6 @@ def binary_search(arr,target):
             right = mid - 1 
         return -1 
     
+arr = [1, 2, 3, 4, 5]    
 print(binary_search(arr, 3))  # Output: 2
 print(binary_search(arr, 6)) 
